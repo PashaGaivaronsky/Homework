6ffcb710dd812f1a9290fa4ef74eb38b5789971c
Pasha Gaivaronsky <pashae53@gmail.com>
2023-10-28 20:26:42 +0300

    Некоторые поправки

--- ДЗ_9.py
+++ ДЗ_9.py
@@ -1,5 +1,6 @@
 print("Домашнее задание")
-lst = [15,48,"hello", 6, 19, "world"]
+lst = [15,48,"hello", 6, 19, "world",]
+summ = []
 glas = 0
 soglas = 0
 for i in lst:
@@ -11,14 +12,15 @@ for i in lst:
             else:
                 soglas += 1
     else:
-        if i % 2 == 0 and i > 9:
+        if i % 2 == 0:
             x = str(i)
-            sum = int(x[0])+int(x[1])
-            print(sum) # Сумма цифр четного числа
-
+            num = map(int,x)
+            s = sum(num)
+            summ.append(s)
         elif i % 2 != 0:
             lst.insert(lst.index(i), 1)
             lst.remove(i)
+print(summ)# Сумма цифр четного числа
 print(f"Согласных:{soglas},Гласных:{glas}") #Количество гласных и согласных
 print(lst) # Замена всех нечетных цифр на еденицу
 
