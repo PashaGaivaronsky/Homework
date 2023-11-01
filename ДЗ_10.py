f47a78b1f82c8ca4c18dd14fb2b57060327a71fc
Pasha Gaivaronsky <pashae53@gmail.com>
2023-11-01 23:28:05 +0300

    Поправки ДЗ_9 и ДЗ_10

---
+++ ДЗ_10.py
@@ -0,0 +1,26 @@
+# print("задание №4")
+# diction = {a : a+1 for a in range(5)}
+# prod_of_val = 1
+# for k , v in diction.items():
+#     prod_of_val *= v
+# print(prod_of_val)
+
+# print("задание №5")
+# l = [ a+1 for a in range(10)]
+# l_2 = [a for a in range(10,30,2)]
+# dictionary = dict(zip(l,l_2))
+# print(dictionary)
+
+# print("задание №6")
+# wrd = "pythonist"
+# keys = []
+# value = []
+# for i in wrd:
+#     keys.append(i)
+#     value.append(wrd.count(i))
+# dictionary = (dict(zip(keys,value)))
+# print(keys)
+# print(value)
+# print(dictionary)
+# # Не понимаю куда пропадает последняя t в словаре
+
--- ДЗ_9.py
+++ ДЗ_9.py
@@ -1,16 +1,16 @@
 print("Домашнее задание")
 lst = [15,48,"hello", 6, 19, "world",]
 summ = []
-glas = 0
-soglas = 0
+vwl = 0
+cnsn = 0
 for i in lst:
     if type(i) == str:
         y = str(i)
         for b in y:
             if b == 'a' or b == 'e' or b == 'i' or b == 'o' or b == 'u':
-                glas += 1
+                vwl += 1
             else:
-                soglas += 1
+                cnsn += 1
     else:
         if i % 2 == 0:
             x = str(i)
@@ -21,6 +21,6 @@ for i in lst:
             lst.insert(lst.index(i), 1)
             lst.remove(i)
 print(summ)# Сумма цифр четного числа
-print(f"Согласных:{soglas},Гласных:{glas}") #Количество гласных и согласных
+print(f"Согласных:{cnsn},Гласных:{vwl}") #Количество гласных и согласных
 print(lst) # Замена всех нечетных цифр на еденицу
 
